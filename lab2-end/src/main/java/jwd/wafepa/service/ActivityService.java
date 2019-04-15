3c520ddc94cda6112e74f037fa1fb60f13cbd6b0
Zeljko Vukovic <zeljkov@uns.ac.rs>
2019-04-15 21:15:54 +0200

    lab1-end

---
+++ lab2-end/src/main/java/jwd/wafepa/service/ActivityService.java
@@ -0,0 +1,13 @@
+package jwd.wafepa.service;
+
+import java.util.List;
+
+import jwd.wafepa.model.Activity;
+
+public interface ActivityService {
+	List<Activity> findAll();
+	Activity findOne(Long id);
+	Activity save(Activity activity);
+	Activity delete(Long id);
+	
+}
